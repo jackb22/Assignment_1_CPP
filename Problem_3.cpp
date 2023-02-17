d712db1edf6ffa33511856a94402f23a93e81600
Jack Bennett <jbennett3@laurentian.ca>
2023-02-17 04:38:55 -0500

    Update Problem_3.cpp

--- Problem_3.cpp
+++ Problem_3.cpp
@@ -1,8 +1,47 @@
-//
-// Created by Jack Bennett on 2023-02-01.
-//
+/*
+ * Title: Problem_2.cpp
+ * Date: January 8th, 2023
+ * Author: Jack Bennett
+ * Version: 1.0
+ */
+
+/* Documentation
+ *
+ * Program Purpose: Create table of temperature  conversions from Fahrenheit to Celsius and vice versa,
+ * starting from -40 to 455.
+ *
+ *
+ * Classes: TemperatureConverter - contains two  static methods CelsiusToFahrenheit( Takes in double Celsius converts to Fahrenheit  and returns double  temp).
+ *          and FahrenheitToCelsius( Takes in  double Fahrenheit converts to Celsius and returns double temp).
+ *
+ * Notes: N/A
+ *
+ * Variables:
+ * Celsius: double -  takes in for loops value (double i) and used to convert to Fahrenheit
+ *
+ * Fahrenheit: double  - takes in for loops value (double i) and used to convert to Fahrenheit .
+ *
+ * i : double - used as counter in for loop to increment by 5 for every iteration of loop / feed in temp to be converted.
+ *
+ *
+ */
+
+
+/*
+ * Test Plan:
+ *
+ * Normal Case:
+ *   Table matches what is presented in the problem.
+ *   Successfully calculate conversions.
+ *
+ * Bad Case :
+ *  Does not produce correct output.
+ *  Does not successfully calculate conversions.
+ *
+ */
+
+
 #include <iostream>
-using namespace std;
 
 //creating class for temp converter
 class TemperatureConverter {
@@ -19,31 +58,21 @@ public:
 };
 
 int main(){
-    // coverting F to C
-
-    // cout for Temp and degree stuff
-    cout<< "\t" <<"\t" << "Temperature" << "\t"<<"\t" <<"\t"  << " |" << "\t"<<"\t" <<"\t"<< "Temperature"
-    << "\n" << "\t" "\t"  << " (Degrees)" << "\t" << "\t" << "\t"  << " |" << "\t" <<  "\t"  << "\t" << " (Degrees)" <<  endl;
-
-    // cout for F to C
-    cout <<"\t" << "  F" << "\t" << "\t" <<"\t "<< "  C" << "\t" << "\t" <<" |" <<"\t" <<"\t" << " C" << "\t" << "\t" <<"\t "<< "  F" << endl;
 
 
+    // cout for the title
+    std::cout<< "\t" <<"\t" << "Temperature" << "\t"<<"\t" <<"\t"  << " |" << "\t"<<"\t" <<"\t"<< "Temperature"
+    << "\n" << "\t" "\t"  << " (Degrees)" << "\t" << "\t" << "\t"  << " |" << "\t" <<  "\t"  << "\t" << " (Degrees)" <<  std::endl;
+
+    // cout for F to C (column)s and C to F (column)s
+    std::cout <<"\t" << "  F" << "\t" << "\t" <<"\t "<< "  C" << "\t" << "\t" <<" |" <<"\t" <<"\t" << " C" << "\t" << "\t" <<"\t "<< "  F" << std::endl;
 
 
+
+    // for loop to print out the table
     for ( double i  = -40 ; i <= 455; i += 5 ){
         std::printf("%10.3f \t %10.3f \t | %10.3f \t %10.3f\n", i, TemperatureConverter::FahrenheitToCelsius(i), i,TemperatureConverter::CelsiusToFahrenheit(i));
-
     }
 
-
-
-
-
-
-
-
-
-};
-
-
+return 0;
+}
